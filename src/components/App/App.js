5da33e677696bb414fae0f5fc14fac742d9bcc5f
Brian Kim <briandkim93@gmail.com>
2018-07-03 19:43:20 -0700

    Added functionality to update search results based on search query value

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import SearchBar from '../SearchBar/SearchBar';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {searchResults: []};
+  }
+  updateSearchResults(query) {
+    axios.get(`http://api.tvmaze.com/search/shows?q=${query}`)
+    .then(response => this.setState({searchResults: response.data}));
+  }
   render() {
     return (
       <div className="app">
-      
+        <SearchBar onSearchTermChange={query => this.updateSearchResults(query)} />
       </div>
     );
   }
