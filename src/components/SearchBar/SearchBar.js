87be27b34efdce740d9695b97555b77380444244
Brian Kim <briandkim93@gmail.com>
2018-07-03 19:39:24 -0700

    Defined controlled SearchBar component that passes search query value to main App component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {query: ''};
+
+    this.updateSearchQuery = this.updateSearchQuery.bind(this);
+  }
+  updateSearchQuery(event) {
+    this.setState({query: event.target.value});
+    this.props.onSearchTermChange(event.target.value);
+  }
+  render() {
+    return (
+      <div>
+        <input 
+          placeholder="Search TV Show"
+          value={this.state.query}
+          onChange={this.updateSearchQuery}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
