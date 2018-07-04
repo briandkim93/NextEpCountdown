e03d2d01f1b887e2818043d527ebcf2f0fcc99e8
Brian Kim <briandkim93@gmail.com>
2018-07-04 12:08:05 -0700

    Implemented logic to clear search results when a show is selected

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,20 +8,29 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {searchResults: [], selectedShowId: ''};
+    this.state = {searchResults: [], selectedShowId: '', showSelected: false};
+    this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
   }
   updateSearchResults(query) {
     axios.get(`http://api.tvmaze.com/search/shows?q=${query}`)
     .then(response => this.setState({searchResults: response.data}));
   }
+  handleSearchTermChange(query) {
+    this.setState({showSelected: false});
+    this.updateSearchResults(query);
+  }
   render() {
     return (
       <div className="app">
         <div className="container my-4">
-          <SearchBar onSearchTermChange={query => this.updateSearchResults(query)} />
+          <SearchBar
+            showSelected={this.state.showSelected}
+            onSearchTermChange={this.handleSearchTermChange} 
+          />
           <ShowList 
             searchResults={this.state.searchResults} 
-            onShowSelect={showId => this.setState({selectedShowId: showId})}
+            showSelected={this.state.showSelected}
+            onShowSelect={showId => this.setState({selectedShowId: showId, showSelected: true})}
           />
         </div>
       </div>
--- src/components/ShowList/ShowList.js
+++ src/components/ShowList/ShowList.js
@@ -3,19 +3,23 @@ import React from 'react';
 import ShowSelection from '../ShowSelection/ShowSelection';
 
 const ShowList = (props) => {
-    const showSelections = props.searchResults.map(
-      searchResult => 
-        <ShowSelection 
-          key={searchResult.show.id} 
-          showInfo={searchResult} 
-          onShowSelect={props.onShowSelect}
-        />
-    );
-  return (
-    <ul className="list-group">
-      {showSelections}
-    </ul>
+  const showSelections = props.searchResults.map(
+    searchResult => 
+      <ShowSelection 
+        key={searchResult.show.id} 
+        showInfo={searchResult} 
+        onShowSelect={props.onShowSelect}
+      />
   );
+  if (props.showSelected) {
+    return <ul></ul>;
+  } else {
+    return (
+      <ul className="list-group">
+        {showSelections}
+      </ul>
+    );
+  }
 };
 
 export default ShowList;
