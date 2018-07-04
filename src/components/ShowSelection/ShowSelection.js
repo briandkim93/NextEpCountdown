fe19e157c0b7c6eaaf31ec6aad87bece364dcbb6
Brian Kim <briandkim93@gmail.com>
2018-07-03 20:59:41 -0700

    Added click event listener to pass up the selected show id

--- src/components/ShowSelection/ShowSelection.js
+++ src/components/ShowSelection/ShowSelection.js
@@ -3,12 +3,16 @@ import React, { Component } from 'react';
 class ShowSelection extends Component {
   constructor(props) {
     super(props);
-
   }
   render() {
     if (this.props.showInfo) {
       return (
-        <li className="list-group-item">{this.props.showInfo.show.name}</li>
+        <li 
+          className="list-group-item" 
+          onClick={() => this.props.onShowSelect(this.props.showInfo.show.id)}
+        >
+          {this.props.showInfo.show.name}
+        </li>
       );
     } else {
       return <li></li>;
