b924a5022a93bd941ac36a3b88d632c99fb49f4b
Brian Kim <briandkim93@gmail.com>
2018-07-04 23:51:05 -0700

    Parsed countdown into days/hours/minutes/seconds

--- src/components/CountDown/CountDown.js
+++ src/components/CountDown/CountDown.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import './CountDown.css';
+
 class CountDown extends Component {
   constructor(props) {
     super(props);
 
     this.state = {secondsLeft: ''};
+  }
+  convertSeconds(n) {
+    const dateObject = {
+      days: Math.floor(n / 86400),
+      hours: Math.floor(n % 86400 / 3600),
+      mins: Math.floor(n % 3600 / 60),
+      secs: Math.floor(n % 60)
+    }
+    return dateObject;
   }
   componentDidUpdate(prevProps) {
     if (this.props.secondsUntilNextEp !== prevProps.secondsUntilNextEp) {
@@ -22,8 +33,26 @@ class CountDown extends Component {
 
   render() {
     if (this.props.secondsUntilNextEp) {
+      const timeLeft = this.convertSeconds(this.state.secondsLeft);
       return (
-        <div>Seconds Left Until Next Episode = {this.state.secondsLeft}</div>
+        <div>
+          <figure className="unit-block">
+            <span className="unit-value">{timeLeft.days}</span>
+            <figcaption>Days</figcaption>
+          </figure>
+          <figure className="unit-block">
+            <span className="unit-value">{timeLeft.hours}</span>
+            <figcaption>Hours</figcaption>
+          </figure>
+          <figure className="unit-block">
+            <span className="unit-value">{timeLeft.mins}</span>
+            <figcaption>Minutes</figcaption>
+          </figure>
+          <figure className="unit-block">
+            <span className="unit-value">{timeLeft.secs}</span>
+            <figcaption>Seconds</figcaption>
+          </figure>
+        </div>
       );
     } else {
       return <div></div>;
