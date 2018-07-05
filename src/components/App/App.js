1117f044f93c01459a6a9acacc87425253c66ac7
Brian Kim <briandkim93@gmail.com>
2018-07-04 18:12:54 -0700

    Added method to handle finding and storing the next episode air time of a selected show

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+import ShowDetail from '../ShowDetail/ShowDetail';
 import SearchBar from '../SearchBar/SearchBar';
 import ShowList from '../ShowList/ShowList';
 
@@ -8,8 +9,14 @@ class App extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {searchResults: [], selectedShowId: '', showSelected: false};
+    this.state = {
+      searchResults: [], 
+      selectedShowId: '', 
+      selectedShow: '',
+      nextEpUnixEpochAirTime: ''
+    };
     this.handleSearchTermChange = this.handleSearchTermChange.bind(this);
+    this.handleOnShowSelect = this.handleOnShowSelect.bind(this);
   }
   updateSearchResults(query) {
     axios.get(`http://api.tvmaze.com/search/shows?q=${query}`)
@@ -19,10 +26,34 @@ class App extends Component {
     this.setState({showSelected: false});
     this.updateSearchResults(query);
   }
+  updateNextEpUnixEpochAirTime(showId) {
+    axios.get(`http://api.tvmaze.com/shows/${showId}/episodes`)
+    .then(
+      response => {
+        const showUnixEpochAirTimes = response.data.map(episode => {
+          const airDateAndTime = new Date(
+            parseInt(episode.airdate.split('-')[0], 0),
+            parseInt(episode.airdate.split('-')[1] - 1, 0),
+            parseInt(episode.airdate.split('-')[2], 0),
+            parseInt(episode.airtime.split(':')[0], 0),
+            parseInt(episode.airtime.split(':')[1], 0)
+          );
+          return airDateAndTime.getTime();
+        });
+        const unairedEpisodesUnixEpochAirTimes = showUnixEpochAirTimes.filter(airTime => airTime >= Date.now());
+        this.setState({nextEpUnixEpochAirTime: unairedEpisodesUnixEpochAirTimes[0]});
+      }
+    );
+  }
+  handleOnShowSelect(show) {
+    this.setState({selectedShow: show, showSelected: true});
+    this.updateNextEpUnixEpochAirTime(show.id);
+  }
   render() {
     return (
       <div className="app">
         <div className="container my-4">
+          <ShowDetail show={this.state.selectedShow} nextEpAirTime={this.state.nextEpUnixEpochAirTime} />
           <SearchBar
             showSelected={this.state.showSelected}
             onSearchTermChange={this.handleSearchTermChange} 
@@ -30,7 +61,7 @@ class App extends Component {
           <ShowList 
             searchResults={this.state.searchResults} 
             showSelected={this.state.showSelected}
-            onShowSelect={showId => this.setState({selectedShowId: showId, showSelected: true})}
+            onShowSelect={this.handleOnShowSelect}
           />
         </div>
       </div>
